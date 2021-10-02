9f7672b19e21c68c1461f6c390ee9bd87e42aec4
Sam Brands <sambrands2001@gmail.com>
2021-10-02 17:42:36 +0200

    Populaire types op pagina's van merken
    Op de pagina van een merk wordt nu (als deze er zijn) een lijst van de 5 populairste types getoond.

--- resources/views/pages/type_list.blade.php
+++ resources/views/pages/type_list.blade.php
@@ -8,6 +8,26 @@
 
 <h1>{{ $brand->name }}</h1>
 
+@if (count($popularTypes) > 0)
+<h2>{{ __('misc.popular_manuals') }}:</h2>
+<div class="container popular-manuals manual-container">
+	<div class="row">
+			<div class="col">
+					<ol></ol>
+					<ol>
+							@foreach($popularTypes as $type)
+							
+							<li>
+									<a href="/{{ $type->brand->id }}/{{ $type->brand->name_url_encoded }}/{{ $type->id }}/{{ $type->name }}" class="type-link">{{ $type->name }}</a>
+							</li>
+							
+							@endforeach
+					</ol>
+			</div>
+	</div>
+</div>
+@endif
+
 <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
 
     <div class="container">
